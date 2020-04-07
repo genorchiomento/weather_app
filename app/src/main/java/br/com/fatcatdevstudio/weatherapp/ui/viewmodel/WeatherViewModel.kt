bd448312770a1e7816e5916f4b1c0c16372ad4b7
Genor Chiomento <genor.chiomento@gmail.com>
2020-04-07 17:02:25 -0300

    add livedata in viewmodel

--- app/src/main/java/br/com/fatcatdevstudio/weatherapp/ui/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/br/com/fatcatdevstudio/weatherapp/ui/viewmodel/WeatherViewModel.kt
@@ -3,8 +3,9 @@ package br.com.fatcatdevstudio.weatherapp.ui.viewmodel
 import android.annotation.SuppressLint
 import android.content.Context
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.fatcatdevstudio.weatherapp.Constants
 import br.com.fatcatdevstudio.weatherapp.R
@@ -18,14 +19,16 @@ import retrofit2.Response
 class WeatherViewModel(
   private val retrofit: InitializeRetrofit
 ) : ViewModel() {
+
+  private val liveData = MutableLiveData<OpenWeatherResponse>()
+
   fun getWeather(
     context: Context,
-    getInputText: EditText,
-    cityResponseTextView: TextView
-  ) {
+    getInputText: EditText
+  ): LiveData<OpenWeatherResponse> {
+
     val inputCityToString = getInputText.text.toString()
-    val service = retrofit
-      .apiService()
+    val service = retrofit.apiService()
     val call = service.getCurrentWeatherByCity(inputCityToString, Constants.API_KEY)
 
     call.enqueue(object : Callback<OpenWeatherResponse> {
@@ -40,30 +43,7 @@ class WeatherViewModel(
       ) {
         if (response?.code() == 200) {
           response.body()?.let { weatherApi ->
-
-            val cityWeather = weatherApi.name
-            val iconWeather = weatherApi.weather[0].icon
-            val weatherDescription = weatherApi.weather[0].description.capitalize()
-            val temperature = weatherApi.main.temp
-            val humidity = weatherApi.main.humidity
-            val feelsLike = weatherApi.main.feelsLike
-            val windSpeed = weatherApi.wind.speed
-
-            val iconUrl = "https://openweathermap.org/img/wn/$iconWeather@2x.png"
-
-            cityResponseTextView.text = cityWeather
-
-//            Picasso.get()
-//              .load(iconUrl)
-//              .into(iconWeatherImageView)
-//
-//            weatherResponseTextView.text = weatherDescription
-//            humidityResponseTextView.text = context.getString(R.string.text_humidity, humidity)
-//            tempResponseTextView.text = context.getString(R.string.text_temperature, temperature)
-//            feelsLikeResponseTextView.text = context.getString(R.string.text_feels_like, feelsLike)
-//            windSpeedResponseTextView.text = context.getString(R.string.text_speed_wind, windSpeed)
-//
-//            editTextCityInput.text.clear()
+            liveData.value = weatherApi
           }
         } else {
           Toast.makeText(
@@ -74,5 +54,7 @@ class WeatherViewModel(
         }
       }
     })
+
+    return liveData
   }
 }
